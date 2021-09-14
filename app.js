44fca15345ebce7ed6ddb3b5c619bb4fbc24cbd7
allahorgolam <52627173+allahorgolam@users.noreply.github.com>
2021-09-14 21:19:18 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,87 +1,85 @@
+const loadProducts = () => {
+  const url = `https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json?fbclid=IwAR3woo0usLwtCnSnQCrJWTovmyoTEXb2xIrFkL2F3TuB2xiUhuHf-Su_aRE`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
+};
+loadProducts();
 
-// function for toggole
- const toggole =(id, property) => {
-     document.getElementById(id).style.display = property;
- }
+// show all product in UI 
+const showProducts = (products) => {
+  const allProducts = products.map((pd) => pd);
+  for (const product of allProducts) {
+    const image = product.image;
+    const div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = `<div class="single-product mt-5 p-4" >
+      <div>
+    <img class="product-image" src=${image}></img>
+      </div>
+      <h3>${product.title.slice(0,14)}</h3>
+      <p class="text-secondary">Category: ${product.category}</p>
+      <p>Raiting: <span style="color: tomato"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star-half-alt"></i></span>(${product.rating.rate})</p>
+      <p class="text-dark">Count: <span style="color: red">${product.rating.count}</span></p>
+      <h2>Price: $ ${product.price}</h2>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-warning mx-1">add to cart</button>
+      <button id="details-btn" class="btn btn-dark">Details</button></div>
+      `;
+    document.getElementById("all-products").appendChild(div);
+  }
+};
+let count = 0;
+const addToCart = (id, price) => {
+  count = count + 1;
+  updatePrice("price", price);
 
-const loadBooks = () => {
-    const searchField = document.getElementById('search-text');
-    const searchText = searchField.value;
-    console.log(searchText);
-    //Clear input field 
-    searchField.value = '';
+  updateTaxAndCharge();
+  document.getElementById("total-Products").innerText = count;
+};
 
-    // toggole 
-    toggole('spinner', 'block')
-    toggole('book-section', 'none')
-    toggole('results-amount', 'none')
- 
-// calling api and load data 
-const url = `https://openlibrary.org/search.json?q=${searchText}`;
-fetch(url)
-.then(res => res.json())
-.then(data => displayBooks(data.docs, data))
-}
+const getInputValue = (id) => {
+  const element = document.getElementById(id).innerText;
+  const converted = parseFloat(element);
+  return converted;
+};
 
-const displayBooks = (books, data) => {
-  
-    // removing nothing found 
-    const NothingFound = document.getElementById('nothing-found');
-    NothingFound.textContent = '';
+// main price update function
+const updatePrice = (id, value) => {
+  const convertedOldPrice = getInputValue(id);
+  const convertPrice = parseFloat(value);
+  const total = convertedOldPrice + convertPrice;
+  document.getElementById(id).innerText = total.toFixed(2);
+  updateTotal()
+};
 
-    // showing results amount
-    const resultAmount = document.getElementById('results-amount');
+// set innerText function
+const setInnerText = (id, value) => {
+  document.getElementById(id).innerText = value.toFixed(2);
+};
 
-    resultAmount.innerHTML = `<h5 style="text-align:center;" class="w-50 mx-auto">About ${data.numFound} results found</h5>`
-    const booksContainer = document.getElementById('books');
+// update delivery charge and total Tax
+const updateTaxAndCharge = () => {
+  const priceConverted = getInputValue("price");
+  if (priceConverted > 200) {
+    setInnerText("delivery-charge", 30);
+    setInnerText("total-tax", priceConverted * 0.2);
+  }
+  if (priceConverted > 400) {
+    setInnerText("delivery-charge", 50);
+    setInnerText("total-tax", priceConverted * 0.3);
+  }
+  if (priceConverted > 500) {
+    setInnerText("delivery-charge", 60);
+    setInnerText("total-tax", priceConverted * 0.4);
+  }
+};
 
+//grandTotal update function
+const updateTotal = () => {
+  const grandTotal =
+    getInputValue("price") + getInputValue("delivery-charge") +
+    getInputValue("total-tax");
+  document.getElementById("total").innerText = grandTotal.toFixed(2)
+};
 
-    // romoving books from container
-    booksContainer.textContent = '';
-
-    // appending books in container 
-   if (books.length > 0){
-        books.forEach(book => {
-            const div = document.createElement("div");
-            div.classList = 'col';
-            div.innerHTML = `
-            <div class="card h-100">
-                        <img class="img-size" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i: 10909258}-M.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h6 class="card-title">${book.title}</h6>
-                          <p class="card-text">Author: ${book.author_name}</p>
-                          <p class="card-text">Published: ${book.first_publish_year}</p>
-                        </div>
-                      </div>
-            `
-            booksContainer.appendChild(div);
-           
-        })
-        
-        
-    }
-    
-    // showing nothing found
-    else if(books.length === 0){
-        const NothingFound = document.getElementById('nothing-found');
-        const div = document.createElement('div');
-        div.innerHTML =`
-        <h1>Nothing found bro</h1> 
-        <h4>No results containing abour your search item.</h4> 
-      <ul>
-        Suggestions:
-        <li>Type your right keyword.</li>
-        <li>Try different keywords.</li>
-        <li>
-            Try fewer keywords.
-            </li>
-      </ul>`
-      NothingFound.appendChild(div)
-    }
-   
-
-    // toggole
-    toggole('spinner', 'none')
-    toggole('book-section', 'block')
-    toggole('results-amount', 'block')
-}
+//--------------------------------------END---------------------------------------//
